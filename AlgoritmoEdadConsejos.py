517f1bdc3894e07b426bd0a038a4838fe25ee054
Mag <migalindor@unal.edu.co>
2025-09-08 12:58:27 -0500

    Cree el codigo para ingresar el nombre

--- AlgoritmoEdadConsejos.py
+++ AlgoritmoEdadConsejos.py
@@ -1,5 +1,11 @@
 #Realizar una encuesta a sus compañeros que contenda estas funciones
 #Si la persona es mayor de 18 años, proporcione un consejo de economia
 #Si la persona es mayor de 20 años, proporcione un consejo de amor
-print("En este codigo, vas a ingresartu nombre y edad,con respecto a eso te dare un consejo")
+print("En este codigo, vas a ingresar tu nombre y edad,con respecto a eso te dare un consejo")
 print(" ")
+Nombre= input("Ingresa tu primer nombre y tu primer apellido: ")
+print (f"Tu nombre es {nombre} verdad?")
+s= int(input("Si quieres corregir tu nombre ingresa 0 ="))
+while (s==0):
+    nombre= input("Digita nuevamente tu nombre: ")
+    s+1
