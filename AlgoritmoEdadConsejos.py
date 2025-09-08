ce83d71391dfde03b7cf4ecb8b6d881ac8365fec
Mag <migalindor@unal.edu.co>
2025-09-08 12:45:08 -0500

    Esta es la primera interfaz

---
+++ AlgoritmoEdadConsejos.py
@@ -0,0 +1,5 @@
+#Realizar una encuesta a sus compañeros que contenda estas funciones
+#Si la persona es mayor de 18 años, proporcione un consejo de economia
+#Si la persona es mayor de 20 años, proporcione un consejo de amor
+print("En este codigo, vas a ingresartu nombre y edad,con respecto a eso te dare un consejo")
+print(" ")
