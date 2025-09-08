f269360cf628ee916b6b53e794ded05838273b26
Mag <migalindor@unal.edu.co>
2025-09-08 12:59:38 -0500

    Corregi un error de variable

--- AlgoritmoEdadConsejos.py
+++ AlgoritmoEdadConsejos.py
@@ -4,7 +4,7 @@
 print("En este codigo, vas a ingresar tu nombre y edad,con respecto a eso te dare un consejo")
 print(" ")
 Nombre= input("Ingresa tu primer nombre y tu primer apellido: ")
-print (f"Tu nombre es {nombre} verdad?")
+print (f"Tu nombre es {Nombre} verdad?")
 s= int(input("Si quieres corregir tu nombre ingresa 0 ="))
 while (s==0):
     nombre= input("Digita nuevamente tu nombre: ")
